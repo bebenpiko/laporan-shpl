3f2200165d12ec96df2d758a1191e2359742edd6
bebenpiko <bebenpiko@yahoo.com>
2013-01-22 06:38:08 +0700

    commit pertama

---
+++ src/main/java/com/benny/laporanshpl/App.java
@@ -0,0 +1,13 @@
+package com.benny.laporanshpl;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
